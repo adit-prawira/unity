9725e8d527856bf9dda159717d76c4fa08b041e2
Aditya Prawira <adityaprawira96@gmail.com>
2021-02-01 20:54:11 +1100

    Add simple codes to read pressed kerboard key

--- NumberWizard/Number Wizard Console/Assets/NumberWizard.cs
+++ NumberWizard/Number Wizard Console/Assets/NumberWizard.cs
@@ -7,15 +7,29 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int min = 1, max = 1000;
+
         Debug.Log("Welcome to Number Wizard");
         Debug.Log("Pick a number");
-        Debug.Log("Highest number you can pick is 1000");
-        Debug.Log("Lowest number you can pick is 1");
+        Debug.Log("Highest number you can pick is: " + max);
+        Debug.Log("Lowest number you can pick is: " + min);
+        Debug.Log("tell me if your number is higher or lower than 500");
+        Debug.Log("Push up = higher, Push down = lower, Push enter = correct");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            Debug.Log("Up");
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow)){
+            Debug.Log("Down");
+        }
+        if(Input.GetKeyDown(KeyCode.Return)){
+            Debug.Log("Enter");
+        }
+
     }
 }
