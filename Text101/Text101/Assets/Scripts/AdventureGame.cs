4a8c69652dbca42e5744f7e20debbfbf63426ca0
Aditya Prawira <adityaprawira96@gmail.com>
2021-02-03 18:10:55 +1100

    Added Story and choices

---
+++ Text101/Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState(){
+        var nextState =state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            state = nextState[0];
+        }else if(Input.GetKeyDown(KeyCode.Alpha2)){
+            state = nextState[1];
+        }else if(Input.GetKeyDown(KeyCode.Alpha3)){
+            state = nextState[2];
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
