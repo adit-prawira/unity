d16c1637235a835523230129d5623d70c187eb6d
Aditya Prawira <adityaprawira96@gmail.com>
2021-02-05 18:10:11 +1100

    Added UI for Number Wizard Game andrefactored some logics

---
+++ NumberWizardGame/Number Wizard Game/Assets/NumberWizard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+        
+    }
+    void StartGame(){
+        max += 1;
+    }
+    public void onPressHigher(){
+        min = guess;
+        NextGuess();
+    }
+    public void onPressLower(){
+        max = guess;
+        NextGuess();
+    }        
+    void NextGuess(){
+        guess = (max + min)/2;
+    }
+}
